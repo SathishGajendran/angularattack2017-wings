7bdb9846d80d1436b17c36fa190fc333e6ba79f6
Surya A <surya.aathiyappan@pearson.com>
2017-04-23 23:12:21 +0530

    Heroku App

--- app/frontend/dashboard/home/home.component.ts
+++ app/frontend/dashboard/home/home.component.ts
@@ -1,25 +1,59 @@
 import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 import initDemo = require('../../assets/js/charts.js');
 
-declare var $:any;
+declare var $: any;
 
 @Component({
     selector: 'home-cmp',
     templateUrl: 'home.component.html'
 })
 
-export class HomeComponent implements OnInit{
-    bmi:any ={
-        index:23,
-        status:"Normal"
+export class HomeComponent implements OnInit {
+    bmiHeight: number;
+    bmiWeight: number;
+    bmi: any = {
+        info: {}
     };
-    ngOnInit(){
-        // $('[data-toggle="checkbox"]').each(function () {
-        //     if($(this).data('toggle') == 'switch') return;
-        //
-        //     var $checkbox = $(this);
-        //     $checkbox.checkbox();
-        // });
-        initDemo();
+
+    ngOnInit() {
+
+    }
+
+    getBMIStatus(value) { console.log(value);
+        if (value < 18.5) {
+            return {
+                className: 'warning',
+                name: 'Underweight'
+            };
+        } else if ((18.5 <= value) && (value <= 24.9)) {
+            return {
+                className: 'primary',
+                name: 'Normal'
+            };
+        } else if ((25 <= value) && (value <= 29.9)) {
+            return {
+                className: 'warning',
+                name: 'Overweight'
+            };
+        } else {
+            return {
+                className: 'danger',
+                name: 'Obesity'
+            };
+        }
+    }
+
+    displayBmiShow() {
+        if (this.bmiHeight && this.bmiWeight) {
+            let data: number = ((this.bmiWeight / this.bmiHeight / this.bmiHeight) * 10000);
+            console.log(data);
+            this.bmi.value = data.toFixed(2);
+            this.bmi.info = this.getBMIStatus(data.toFixed(2));
+            console.log(this.getBMIStatus(data.toFixed(2)));
+            console.log('--:')
+            return true;
+        } else {
+            return false;
+        }
     }
 }
