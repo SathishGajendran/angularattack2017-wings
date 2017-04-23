b218484bad2c9958b71eb93ae9ed48f83e2d5391
Sathish G <sathish.gajendran@gmail.com>
2017-04-23 22:40:52 +0530

    menu changes

--- app/frontend/sidebar/sidebar-routes.config.ts
+++ app/frontend/sidebar/sidebar-routes.config.ts
@@ -3,10 +3,5 @@ import {  RouteInfo } from './sidebar.metadata';
 export const ROUTES: RouteInfo[] = [
     { path: '', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: 'user', title: 'User Profile',  icon:'person', class: '' },
-    { path: 'table', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: 'mygoals', title: 'My Goals',  icon:'library_books', class: '' },
-    { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    // { path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
-    // { path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    // { path: 'upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    { path: 'mygoals', title: 'My Goals',  icon:'library_books', class: '' }
 ];
