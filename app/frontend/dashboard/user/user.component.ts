83ab57c3cee3d13839a67805764c57f8c745618b
Sathish G <sathish.gajendran@gmail.com>
2017-04-23 13:50:19 +0530

    user page updated

--- app/frontend/dashboard/user/user.component.ts
+++ app/frontend/dashboard/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShardService } from '../../shared/shared.service';
 
 @Component({
     selector: 'user-cmp',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class UserComponent implements OnInit{
+    user:object;
+    
+    constructor(shardService:ShardService){
+        this.user = shardService.user;
+    }
     ngOnInit(){
         // $.getScript('../../../assets/js/material-dashboard.js');
 
     }
+
+
 }
---
+++ app/frontend/shared/shared.service.ts
@@ -0,0 +1,88 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+
+export class ShardService {
+    user = {
+        "id": "105580958176713100797",
+        "displayName": "Sathish G",
+        "name": {
+            "familyName": "G",
+            "givenName": "Sathish"
+        },
+        "emails": [
+            {
+                "value": "sathish.gajendran@gmail.com",
+                "type": "account"
+            }
+        ],
+        "photos": [
+            {
+                "value": "https://lh4.googleusercontent.com/-YlqWEuWTd_c/AAAAAAAAAAI/AAAAAAAAFzs/vn7z5O-GH" +
+                    "fk/photo.jpg?sz=50"
+            }
+        ],
+        "gender": "male",
+        "provider": "google",
+        "_raw": "{\n \"kind\": \"plus#person\",\n \"etag\": \"\\\"Sh4n9u6EtD24TM0RmWv7jTXojqc/CbX" +
+                "Y3nx4MRVn1c6ZgJu9qWlOLLQ\\\"\",\n \"occupation\": \"Software Engineer\",\n \"gen" +
+                "der\": \"male\",\n \"emails\": [\n  {\n   \"value\": \"sathish.gajendran@gmail.c" +
+                "om\",\n   \"type\": \"account\"\n  }\n ],\n \"urls\": [\n  {\n   \"value\": \"ht" +
+                "tp://sathishgajendran.github.io/\",\n   \"type\": \"otherProfile\",\n   \"label" +
+                "\": \"My Profile\"\n  }\n ],\n \"objectType\": \"person\",\n \"id\": \"105580958" +
+                "176713100797\",\n \"displayName\": \"Sathish G\",\n \"name\": {\n  \"familyName" +
+                "\": \"G\",\n  \"givenName\": \"Sathish\"\n },\n \"url\": \"https://plus.google.c" +
+                "om/+SathishG44\",\n \"image\": {\n  \"url\": \"https://lh4.googleusercontent.com" +
+                "/-YlqWEuWTd_c/AAAAAAAAAAI/AAAAAAAAFzs/vn7z5O-GHfk/photo.jpg?sz=50\",\n  \"isDefa" +
+                "ult\": false\n },\n \"organizations\": [\n  {\n   \"name\": \"T.J.S Engineering " +
+                "College\",\n   \"type\": \"school\",\n   \"primary\": false\n  }\n ],\n \"isPlus" +
+                "User\": true,\n \"language\": \"en\",\n \"circledByCount\": 16,\n \"verified\": " +
+                "false\n}\n",
+        "_json": {
+            "kind": "plus#person",
+            "etag": "\"Sh4n9u6EtD24TM0RmWv7jTXojqc/CbXY3nx4MRVn1c6ZgJu9qWlOLLQ\"",
+            "occupation": "Software Engineer",
+            "gender": "male",
+            "emails": [
+                {
+                    "value": "sathish.gajendran@gmail.com",
+                    "type": "account"
+                }
+            ],
+            "urls": [
+                {
+                    "value": "http://sathishgajendran.github.io/",
+                    "type": "otherProfile",
+                    "label": "My Profile"
+                }
+            ],
+            "objectType": "person",
+            "id": "105580958176713100797",
+            "displayName": "Sathish G",
+            "name": {
+                "familyName": "G",
+                "givenName": "Sathish"
+            },
+            "url": "https://plus.google.com/+SathishG44",
+            "image": {
+                "url": "https://lh4.googleusercontent.com/-YlqWEuWTd_c/AAAAAAAAAAI/AAAAAAAAFzs/vn7z5O-GH" +
+                        "fk/photo.jpg?sz=50",
+                "isDefault": false
+            },
+            "organizations": [
+                {
+                    "name": "T.J.S Engineering College",
+                    "type": "school",
+                    "primary": false
+                }
+            ],
+            "isPlusUser": true,
+            "language": "en",
+            "circledByCount": 16,
+            "verified": false
+        },
+        "userToken": "ya29.Gls1BCKm99D3eCo7MJKavLWW6Pbht5FqRb88FJEyxUsIw50vMxooq9mBh1G2wOj6S1iHe_gLbd_" +
+                "tCoo3l5OTQSwct7xAVx3lisXK4hIQIWjtT_hzUt4WJRyn4m4D"
+
+    };
+}
